283d515e94b48ef9016dae07a4c30997ceff8b68
jo <ljonas@riseup.net>
2026-09-21 00:50:10 +0200

    fix: replace deprecated strftime() with date()
    Related to https://github.com/libretime/libretime/pull/3240

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -1019,7 +1019,14 @@ abstract class " . $this->getClassname() . " extends " . $parentClass . " ";
         }
 
         if (strpos(\$format, '%') !== false) {
-            throw new PropelException('strftime format not supported anymore');
+            return date(strtr(\$format, [
+                '%Y' => 'Y', '%y' => 'y', '%m' => 'm', '%d' => 'd',
+                '%e' => 'j', '%H' => 'H', '%k' => 'G', '%I' => 'h', '%l' => 'g',
+                '%M' => 'i', '%S' => 's', '%A' => 'l', '%a' => 'D', '%B' => 'F',
+                '%b' => 'M', '%p' => 'A', '%P' => 'a', '%Z' => 'T', '%z' => 'O',
+                '%x' => 'Y-m-d', '%X' => 'H:i:s',
+                '%w' => 'w', '%u' => 'N', '%j' => 'z', '%%' => '%',
+            ]), (int) \$dt->format('U'));
         }
 
         return \$dt->format(\$format);
